ebef835696b86dfc3f5f4e3de5560c408084d05e
shehan1215 <kaushikashehan123@gmail.com>
2024-04-16 10:17:49 +0530

    thunder client setup to clarify the req and res

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
+
 const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res)=>{
+    res.status(200).json({message:"All user's contacts"});
+});
 
 app.listen(port, ()=>{
     console.log(`Run the port No: ${port}`);
