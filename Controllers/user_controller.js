ab9e2947ad92021dc6c08031eb2d154c4d5499f6
shehan1215 <kaushikashehan123@gmail.com>
2024-04-17 15:18:39 +0530

    add the password hashing and check the user registerd. To verify the users start to add jwt. to implement that install the node package jsonwebtoken

--- Controllers/user_controller.js
+++ Controllers/user_controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const availableUser = require("../models/userModel");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jasonwebtoken");
 
 //@route GET /api/register
 const register = asyncHandler(async(req,res)=>{
@@ -9,15 +10,38 @@ const register = asyncHandler(async(req,res)=>{
         res.status(400);
         throw new Error("Please fill the all fields");
     }
-    const user = await availableUser.findOne({email});
-    if(user){
+//check the already regsterd user or not    
+    const userVerified = await availableUser.findOne({email});
+    if(userVerified){
         res.status(400);
         throw new Error("error: Email is already exist!");
     }
+    const hashedPw = await bcrypt.hash(password, 10);
+    console.log("Password: ", hashedPw);
+    const user = await availableUser.create({
+        email,
+        password:hashedPw,
+        confirmPassword,
+    });
+    console.log(`Created Successfully ${user}`);
+    if(user){
+        res.status(201).json({_id:user.id, email:user.email});
+    }else{
+        res.status(400);
+        throw new Error("Not valid Data!");
+    }
+
     res.json({message:"Register to the Contact Portal"}); 
 });
 //@route GET /api/login
 const login = asyncHandler(async(req,res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+    res.status(400);
+    throw new Error("Please fill the all fields");
+    }
+    const user = await availableUser.findOne({email});
+    
     res.json({message:"Login"});
 });
 //@route GET /api/current
